6e014c442316cefb90b917f4d79a47e96e99ce94
Jaden Law <jadenlaw4@gmail.com>
2024-06-12 13:52:35 -0400

    Added EntryResponse again (was deleted)

---
+++ src/main/java/com/budgeteers/financetracker/api/controllers/entry/models/EntryResponse.java
@@ -0,0 +1,14 @@
+package com.budgeteers.financetracker.api.controllers.entry.models;
+
+import lombok.Getter;
+
+@Getter
+public class EntryResponse {
+    private final int status;
+    private final String message;
+
+    public EntryResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
